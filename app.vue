9bffb2cfa9a4333891a558da2d47e2fc14b6acfa
junghwan kim <kjh2@torder.co.kr>
2022-08-08 14:50:00 +0900

    asdsadads

--- app.vue
+++ app.vue
@@ -8,12 +8,18 @@
   <button @click="()=>{count+=1}">
     증가
   </button>
+  <Sample v-if="flag"/>
+  <button @click="()=>{flag = !flag}">쇼미 컴포넌트</button>
 </template>
 
 <script lang="ts" setup>
 
+import Sample from './components/Sample.vue'
+
 const count = ref(1);
 
 const double = computed(()=>count.value*2);
+
+const flag = ref(false);
 
 </script>
---
+++ components/Sample.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    햐햐햐햫이
+  </div>
+</template>
