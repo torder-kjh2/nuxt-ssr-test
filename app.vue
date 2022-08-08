7490bee7bd27b9cef1429c47ffad5b40be6fd8aa
junghwan kim <kjh2@torder.co.kr>
2022-08-08 14:37:38 +0900

    test

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{count}}
+  </div>
+  <div>
+    {{double}}
+  </div>
+  <button @click="()=>{count+=1}">
+    증가
+  </button>
+</template>
+
+<script lang="ts" setup>
+
+const count = ref(1);
+
+const double = computed(()=>count.value*2);
+
+</script>
