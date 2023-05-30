70426716995097319685f340ad11d170b3641b68
Padd <kjh2@torder.co.kr>
2023-05-30 12:40:41 +0900

    [fix] nuxt 3.5로 업그레이드

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
 })
-
